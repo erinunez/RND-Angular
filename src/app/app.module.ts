9ac7977b5d7c240cb825827de7d88b7671184334
Erinuneh <zaireenakhzar@gmail.com>
2023-11-24 15:51:53 +0800

    binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule  } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +23,7 @@ import { EventsHandlingComponent } from './events-handling/events-handling.compo
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/events-handling/events-handling.component.ts
+++ src/app/events-handling/events-handling.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-events-handling',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class EventsHandlingComponent {
 
+  valueInput:string ='hello'
+  
+  twoWayBinding:any;
+
+  constructor(private formModule: FormsModule){}
+
+  buttonClick(){  
+  alert("Hello! I am an alert box!");
+  }
+  
+  onKeyup(event: any) {
+    this.valueInput = event.target.value;
+    console.log(this.valueInput)
+  }
+  onKeyupData(a: any){
+    console.log(a)
+  }
+  onKeyupDataElse() {
+    console.log(this.twoWayBinding)
+  }
 }
